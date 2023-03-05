05e22dd15da7c37dd31e17568ede6146e05a1de7
NikitaAlekseevich <tnikitaalekseevich@gmail.com>
2023-03-05 16:49:02 +0300

    Final version | Change default icon | View Binding method | Vertical/Horizontal optimization

--- app/src/main/java/com/example/masscalculator/MainActivity.java
+++ app/src/main/java/com/example/masscalculator/MainActivity.java
@@ -3,28 +3,24 @@ package com.example.masscalculator;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.masscalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText inputEditText;
-    private TextView resultTextView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        inputEditText = findViewById(R.id.inputEditText);
-        resultTextView = findViewById(R.id.resultTextView);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // установка обработчика событий на кнопку "Рассчитать"
-        Button calculateButton = findViewById(R.id.calculateButton);
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculate();
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     // метод для расчета значений в других единицах измерения
     private void calculate() {
         // получение входного значения от пользователя
-        String inputString = inputEditText.getText().toString().trim();
+        String inputString = binding.inputEditText.getText().toString().trim();
         if (inputString.isEmpty()) {
             Toast.makeText(this, "@string/enterValue_text", Toast.LENGTH_SHORT).show();
             return;
@@ -54,6 +50,6 @@ public class MainActivity extends AppCompatActivity {
         // форматирование результатов и вывод в текстовое поле
         @SuppressLint("DefaultLocale") String resultString = String.format("Караты: %.2f\nГраммы: %.2f\nКилограммы: %.2f\nЦентнеры: %.2f\nТонны: %.2f\nФунты: %.2f\nУнции: %.2f",
                 karats, grams, kilograms, centners, tons, pounds, ounces);
-        resultTextView.setText(resultString);
+        binding.resultTextView.setText(resultString);
     }
 }
